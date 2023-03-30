712dc667b27bf8b3cba8517a903f2a377f33b518
Valimp <valentinchauvet.contact@gmail.com>
2023-03-30 03:36:57 +0200

    Decoration and animation on main

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -2,10 +2,12 @@ const burger = document.getElementById('burger-button')
 const links = document.getElementById('nav-links')
 let burgerState = false
 
+// Change state of burger button
 const handleBurgerState = () => {
     burgerState = !burgerState
 }
 
+// onClick => add or remove 'show-links' class to burger-button id element
 burger.addEventListener('click', handleBurgerState)
 burger.addEventListener('click', () => {
     if (burgerState) {
